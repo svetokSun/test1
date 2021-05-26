6254d2bab2fd3252ac0ddfe431c67c7c5b7d9c05
svetokSun <79097116+svetokSun@users.noreply.github.com>
2021-05-26 18:11:31 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+$(function(){
+  $.fn.followTo = function (pos) {
+    var $this = this,
+        $window = $(window);
+    $window.scroll(function (e) {
+        if ($window.scrollTop() > pos) {
+            $this.css({
+                position: 'fixed',
+                top: 0,
+                bottom: 0,
+                right: 0,
+                left: 0
+            });
+        } else {
+            $this.css({
+                position: 'absolute',
+                top: 0,
+                bottom: 0,
+                right: 0,
+                left: 0
+            });
+        }
+    });
+};
+$('.background, .float-block').followTo(1);
+});
